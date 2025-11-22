09e191bf32a16b192ee8eb6dd41adc3702d35607
Jake Martin <jm9357481@gmail.com>
2025-11-22 14:57:57 -0800

    lib/Rules.ml: Apply tag, ssd and float rules

---
+++ lib/Rules.ml
@@ -0,0 +1,19 @@
+let rule_add = [ "riverctl"; "rule-add"; "-app-id" ]
+
+(** Add tag rule for app-id *)
+let tag_rule tuple =
+  let command = rule_add @ [ fst tuple; "tags"; snd tuple -. 1. |> Util.pow  ] in
+  Util.apply command
+;;
+
+(** Add ssd rule for app-id *)
+let ssd_rule app_id =
+  let command = rule_add @ [ app_id; "ssd" ] in
+  Util.apply command
+;;
+
+(** Add float rule for app-id *)
+let float_rule app_id =
+  let command = rule_add @ [ app_id; "float" ] in
+  Util.apply command
+;;
